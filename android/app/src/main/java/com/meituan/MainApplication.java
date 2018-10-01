cca542db0c729da04bf9ff2207d59271fcd40fcf
mengyang <166463126@qq.com>
2018-10-01 14:38:51 +0800

    android 修改头像

--- android/app/src/main/java/com/meituan/MainApplication.java
+++ android/app/src/main/java/com/meituan/MainApplication.java
@@ -3,6 +3,8 @@ package com.meituan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.mehcode.reactnative.splashscreen.SplashScreenPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,6 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
             new SplashScreenPackage()
       );
     }
