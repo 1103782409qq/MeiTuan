762a6006dff8179f09c305fbadcb35a4c47f6f5d
mengyang <166463126@qq.com>
2018-10-04 15:24:51 +0800

    android popover改成es6的方式写

--- src/scene/Home/QrCodeScanner.js
+++ src/scene/Home/QrCodeScanner.js
@@ -1,55 +1,11 @@
-import createReactClass from 'create-react-class';
-
-var React = require('react');
-var Popover = require('react-native-popover');
-var {
-    AppRegistry,
+import React from "react"
+import { AppRegistry,
     StyleSheet,
     Text,
     TouchableHighlight,
-    View,
-} = require('react-native');
-
-var QrCodeScanner = createReactClass({
-    getInitialState() {
-        return {
-            isVisible: false,
-            buttonRect: {},
-        };
-    },
-
-    showPopover() {
-        this.refs.button.measure((ox, oy, width, height, px, py) => {
-            this.setState({
-                isVisible: true,
-                buttonRect: {x: px, y: py, width: width, height: height}
-            });
-        });
-    },
-
-    closePopover() {
-        this.setState({isVisible: false});
-    },
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <TouchableHighlight ref='button' style={styles.button} onPress={this.showPopover}>
-                    <Text style={styles.buttonText}>Press me</Text>
-                </TouchableHighlight>
-
-                <Popover
-                    isVisible={this.state.isVisible}
-                    fromRect={this.state.buttonRect}
-                    onClose={this.closePopover}>
-                    <Text>I'm the content of this popover!</Text>
-                </Popover>
-            </View>
-        );
-    }
-});
-
-var styles = StyleSheet.create({
+    View } from "react-native"
+import Popover from 'react-native-popover'
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
@@ -67,6 +23,45 @@ var styles = StyleSheet.create({
     },
     buttonText: {
     }
-});
+})
 
+class QrCodeScanner extends React.Component {
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+            isVisible: false,
+            buttonRect: {},
+        }
+
+    }
+    showPopover() {
+        this.refs.button.measure((ox, oy, width, height, px, py) => {
+            this.setState({
+                isVisible: true,
+                buttonRect: {x: px, y: py, width: width, height: height}
+            });
+        });
+    }
+
+    closePopover() {
+        this.setState({isVisible: false});
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableHighlight ref='button' style={styles.button} onPress={this.showPopover.bind(this)}>
+                    <Text style={styles.buttonText}>Press me</Text>
+                </TouchableHighlight>
+
+                <Popover
+                    isVisible={this.state.isVisible}
+                    fromRect={this.state.buttonRect}
+                    onClose={this.closePopover.bind(this)}>
+                    <Text>I'm the content of this popover!</Text>
+                </Popover>
+            </View>
+        )
+    }
+}
 export default QrCodeScanner
