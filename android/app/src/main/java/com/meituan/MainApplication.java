4fa12341939e2b11f9e02b515ac0b448e6f8c699
mengyang <166463126@qq.com>
2018-10-04 19:15:21 +0800

    android popover-menu

--- android/app/src/main/java/com/meituan/MainApplication.java
+++ android/app/src/main/java/com/meituan/MainApplication.java
@@ -3,6 +3,7 @@ package com.meituan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import ui.popovermenu.RNPopoverMenuPackage;
 import cn.qiuxiang.react.geolocation.AMapGeolocationPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPopoverMenuPackage(),
             new AMapGeolocationPackage(),
             new RNCameraPackage(),
             new VectorIconsPackage(),
--- src/scene/Home/QrCodeScanner.js
+++ src/scene/Home/QrCodeScanner.js
@@ -1,67 +1,178 @@
-import React from "react"
-import { AppRegistry,
+import React, { Component } from 'react';
+import {
+    Platform,
     StyleSheet,
     Text,
+    View,
     TouchableHighlight,
-    View } from "react-native"
-import Popover from 'react-native-popover'
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgb(43, 186, 180)',
-    },
-    button: {
-        borderRadius: 4,
-        padding: 10,
-        marginLeft: 10,
-        marginRight: 10,
-        backgroundColor: '#ccc',
-        borderColor: '#333',
-        borderWidth: 1,
-    },
-    buttonText: {
-    }
-})
+    ImageBackground
+} from "react-native";
 
-class QrCodeScanner extends React.Component {
-    constructor(props: Props) {
+import RNPopover from 'react-native-popover-menu'
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import Top from "./components/Top";
+import Center from "./components/Center";
+import Bottom from "./components/Bottom";
+
+
+export default class QrCodeScanner extends Component<{}> {
+    constructor (props) {
         super(props)
 
         this.state = {
-            isVisible: false,
-            buttonRect: {},
+            visible: false
+        }
+    }
+
+    _onPress = (ref) => {
+        let copy = <Icon name="copy" size={30} color="#000000" family={"FontAwesome"} />;
+        let paste = <Icon name="paste" size={30} color="#000000" family={"FontAwesome"} />;
+        let share = <Icon name="share" size={30} color="#000000" family={"FontAwesome"} />;
+
+        let meunsIOS =
+            [{
+                label: "Editing",
+                menus: [
+                    {
+                        label: "Copy",
+                        icon: copy
+                    },
+                    {
+                        label: "Paste",
+                        icon: 'paste.png'
+                    },
+                    {
+                        label: "Share",
+                        icon: share
+                    },
+                    {
+                        label: "Share me please"
+                    }
+                ]
+            }]
+
+        let menusAndroid = [
+            {
+                label: "Editing",
+                menus: [
+                    {
+                        label: "Copy",
+                        icon: copy
+                    },
+                    {
+                        label: "Paste",
+                        icon: paste
+                    }
+                ]
+            },
+            {
+                label: "Other",
+                menus: [
+                    {
+                        label: "Share",
+                        icon: share
+                    }
+                ]
+            },
+            {
+                label: "",
+                menus: [
+                    {
+                        label: "Share me please"
+                    }
+                ]
+            }
+        ]
+
+        let menus
+        if (Platform.OS === 'android') {
+            menus = menusAndroid;
+        } else if (Platform.OS === 'ios') {
+            menus = meunsIOS;
         }
 
-    }
-    showPopover() {
-        this.refs.button.measure((ox, oy, width, height, px, py) => {
-            this.setState({
-                isVisible: true,
-                buttonRect: {x: px, y: py, width: width, height: height}
-            });
-        });
+        RNPopover.Show(ref, { menus: menus, onDone: selection => {
+                console.log("selected item index: " + selection);
+            }, onCancel: () => {
+                console.log("popover canceled");
+            }, tintColor: "#888888", textColor: "#FFFFFF" });
     }
 
-    closePopover() {
-        this.setState({isVisible: false});
+    _show (ref) {
+        this.ref = ref
+
+        this.setState({
+            visible: true
+        })
     }
+
     render() {
-        return (
-            <View style={styles.container}>
-                <TouchableHighlight ref='button' style={styles.button} onPress={this.showPopover.bind(this)}>
-                    <Text style={styles.buttonText}>Press me</Text>
-                </TouchableHighlight>
+        let copy = <Icon name="copy" size={30} color="#000000" family={"FontAwesome"} />;
+        let paste = <Icon name="paste" size={30} color="#000000" family={"FontAwesome"} />;
+        let share = <Icon name="share" size={30} color="#000000" family={"FontAwesome"} />;
 
-                <Popover
-                    isVisible={this.state.isVisible}
-                    fromRect={this.state.buttonRect}
-                    onClose={this.closePopover.bind(this)}>
-                    <Text>I'm the content of this popover!</Text>
-                </Popover>
-            </View>
-        )
+        let popover;
+        if (Platform.OS === 'android') {
+            popover = <RNPopover visible={this.state.visible} reference={this.ref} onDone={(mainMenuSelection, subMenuSelection) => {
+                console.log("selection: " + mainMenuSelection + ", " + subMenuSelection);
+            }}>
+                <RNPopover.Menu label={"Editing"}>
+                    <RNPopover.Menu label={"Copy"} icon={copy} />
+                    <RNPopover.Menu label={"Paste"} icon={paste} />
+                </RNPopover.Menu>
+                <RNPopover.Menu>
+                    <RNPopover.Menu label={"Share"} icon={share} />
+                </RNPopover.Menu>
+            </RNPopover>;
+        } else if (Platform.OS === 'ios') {
+            popover = <RNPopover visible={this.state.visible} reference={this.ref} onDone={(selection) => {
+                console.log("selection: " + selection);
+            }} >
+                <RNPopover.Menu label={"Editing"}>
+                    <RNPopover.Menu label={"Copy"} icon={copy} />
+                    <RNPopover.Menu label={"Paste"} icon={paste} />
+                    <RNPopover.Menu label={"Share"} icon={share} />
+                </RNPopover.Menu>
+            </RNPopover>;
+        }
+
+        return <ImageBackground source={require("./assets/dark.jpg")} style={styles.backgroundImage}>
+            <Top style={styles.top} onPress={ref => {
+                this._onPress(ref);
+                // this._show(ref);
+            }} />
+            <Center style={styles.center} onPress={ref => {
+                this._onPress(ref);
+                // this._show(ref);
+            }} />
+            <Bottom style={styles.bottom} onPress={ref => {
+                this._onPress(ref);
+                // this._show(ref);
+            }} />
+            {popover}
+        </ImageBackground>;
     }
 }
-export default QrCodeScanner
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        flexDirection: "column",
+        justifyContent: "space-between"
+    },
+    textStyle: {
+        color: "#FFFFFF"
+    },
+    top: {
+        flex: 1
+    },
+    center: {
+        flex: 1
+    },
+    bottom: {
+        flex: 1
+    }
+});
