d2705ed8f52e47b25bfa5ec951f26d38d20e2e3e
mengyang <166463126@qq.com>
2018-10-17 15:40:30 +0800

    android react-native-modal 底部弹出 分享样式

--- src/scene/Mine/WechatShare.js
+++ src/scene/Mine/WechatShare.js
@@ -4,8 +4,11 @@ import {
     StyleSheet,
     Text,
     TouchableOpacity,
-    View
+    View,
+    Button
 } from "react-native";
+import {Dimensions} from 'react-native'
+
 import Modal from "react-native-modal";
 
 export default class WechatShare extends Component {
@@ -49,17 +52,39 @@ export default class WechatShare extends Component {
                     style={styles.bottomModal}
                 >
                     <View style={styles.scrollableModal}>
-                        <ScrollView horizontal={true}
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={ false }
                             ref={ref => (this.scrollViewRef = ref)}
                             onScroll={this.handleOnScroll}
                         >
                             <View style={styles.scrollableModalContent1}>
-                                <Text>Scroll me up</Text>
+                                <Text style={styles.fontCss}>&#xe600;</Text>
+                                <Text>微信好友</Text>
                             </View>
-                            <View style={styles.scrollableModalContent2}>
-                                <Text>Scroll me up2</Text>
+                            <View style={styles.scrollableModalContent1}>
+                                <Text style={styles.fontCss}>&#xe601;</Text>
+                                <Text>微信朋友圈</Text>
+                            </View>
+                            <View style={styles.scrollableModalContent1}>
+                                <Text style={styles.fontCss}>&#xe641;</Text>
+                                <Text>QQ好友</Text>
+                            </View>
+                            <View style={styles.scrollableModalContent1}>
+                                <Text style={styles.fontCss}>&#xe674;</Text>
+                                <Text>QQ空间</Text>
+                            </View>
+
+                            <View style={styles.scrollableModalContent1}>
+                                <Text style={styles.fontCss}>&#xe61f;</Text>
+                                <Text>系统分享</Text>
+                            </View>
+                            <View style={styles.scrollableModalContent1}>
+                                <Text style={styles.fontCss}>&#xe744;</Text>
+                                <Text>复制链接</Text>
                             </View>
                         </ScrollView>
+                        <TouchableOpacity style={styles.cancelToucha} onPress={() => this.setState({ visibleModal: null })}>
+                            <Text style={styles.cancelText}>取消</Text>
+                        </TouchableOpacity>
                     </View>
                 </Modal>
             </View>
@@ -98,17 +123,26 @@ const styles = StyleSheet.create({
         height: 200
     },
     scrollableModalContent1: {
-        height: 200,
-        width:300,
-        backgroundColor: "orange",
+        height: 150,
+        width:Dimensions.get('window').width/4,
+        backgroundColor: "#f2f2f2",
         alignItems: "center",
         justifyContent: "center"
     },
-    scrollableModalContent2: {
-        height: 200,
-        width:300,
-        backgroundColor: "red",
+    fontCss:{
+        fontFamily:'iconfont',
+        fontSize:50
+    },
+    cancelToucha:{
+        backgroundColor:'#f8f8f8',
+        height:50,
         alignItems: "center",
         justifyContent: "center"
+    },
+    cancelText:{
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: 'bold'
     }
+
 });
