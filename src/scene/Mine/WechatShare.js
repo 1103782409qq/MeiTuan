a665e5cad74f27a217b1989d824ac49e95398934
mengyang <166463126@qq.com>
2018-10-17 17:07:33 +0800

    android react-native-modal 底部弹出 分享样式 集成分享功能，系统分享，复制连接

--- src/scene/Mine/WechatShare.js
+++ src/scene/Mine/WechatShare.js
@@ -1,20 +1,32 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
     ScrollView,
     StyleSheet,
     Text,
     TouchableOpacity,
     View,
-    Button
+    Button,
+    Clipboard
 } from "react-native";
 import {Dimensions} from 'react-native'
 
 import Modal from "react-native-modal";
+import JShareModule from 'jshare-react-native';
+import Share from 'react-native-share';
 
 export default class WechatShare extends Component {
     state = {
-        visibleModal: null
+        visibleModal: null,
+        text:'我是文本'
     };
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        JShareModule.setDebug({enable: true})
+    }
 
     renderButton = (text, onPress) => (
         <TouchableOpacity onPress={onPress}>
@@ -36,53 +48,155 @@ export default class WechatShare extends Component {
             this.scrollViewRef.scrollTo(p);
         }
     };
+    onSharePress1 = () => {
+        var shareParam = {
+            platform: "wechat_session",
+            // type: "image",
+            // imagePath: "/storage/emulated/0/DCIM/Camera/IMG20170707202330.jpg"
+            type: "link",
+            url: "https://jiguang.cn",
+            text: "JShare test text2",
+        };
+        JShareModule.share(shareParam, (map) => {
+            alert(map)
+            console.log("share result: " + JSON.stringify(map));
+            this.setState({visibleModal: null})
+        }, (map) => {
+            alert(map)
+            console.log(map);
+        });
+    }
+    onSharePress2 = () => {
+        var shareParam = {
+            platform: "wechat_timeLine",
+            // type: "image",
+            // imagePath: "/storage/emulated/0/DCIM/Camera/IMG20170707202330.jpg"
+            type: "link",
+            url: "https://jiguang.cn",
+            text: "JShare test text2",
+        };
+        JShareModule.share(shareParam, (map) => {
+            alert(map)
+            console.log("share result: " + JSON.stringify(map));
+            this.setState({visibleModal: null})
+        }, (map) => {
+            alert(map)
+            console.log(map);
+        });
+    }
+    onSharePress3 = () => {
+        var shareParam = {
+            platform: "qq",
+            // type: "image",
+            // imagePath: "/storage/emulated/0/DCIM/Camera/IMG20170707202330.jpg"
+            type: "link",
+            url: "https://jiguang.cn",
+            text: "JShare test text2",
+        };
+        JShareModule.share(shareParam, (map) => {
+            alert(map)
+            console.log("share result: " + JSON.stringify(map));
+            this.setState({visibleModal: null})
+        }, (map) => {
+            alert(map)
+            console.log(map);
+        });
+    }
+    onSharePress4 = () => {
+        var shareParam = {
+            title: 'qzone share',
+            platform: "qzone",
+            // type: "image",
+            // imagePath: "/storage/emulated/0/DCIM/Camera/IMG20170707202330.jpg"
+            type: "link",
+            url: "https://jiguang.cn",
+            text: "JShare test text2",
+        };
+        JShareModule.share(shareParam, (map) => {
+            alert(map)
+            console.log("share result: " + JSON.stringify(map));
+            this.setState({visibleModal: null})
+        }, (map) => {
+            alert(map)
+            console.log(map);
+        });
+    }
+    onSharePress5 = () => {
+        const shareOptions = {
+            title: "React Native",
+            message: "Hola mundo",
+            url: "http://facebook.github.io/react-native/",
+            subject: "Share Link"
+        };
+        return Share.open(shareOptions);
+    }
+
+    async onSharePress6() {
+        Clipboard.setString(this.state.text);
+        let str = await Clipboard.getString()
+        console.log(str)//我是文本
+    }
 
     render() {
         return (
             <View style={styles.container}>
 
                 {this.renderButton("Scrollable modal", () =>
-                    this.setState({ visibleModal: 8 })
+                    this.setState({visibleModal: 8})
                 )}
 
                 <Modal
                     isVisible={this.state.visibleModal === 8}
-                    onBackdropPress={() => this.setState({ visibleModal: null })}
+                    onBackdropPress={() => this.setState({visibleModal: null})}
                     scrollTo={this.handleScrollTo}
                     style={styles.bottomModal}
                 >
                     <View style={styles.scrollableModal}>
-                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={ false }
-                            ref={ref => (this.scrollViewRef = ref)}
-                            onScroll={this.handleOnScroll}
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}
+                                    ref={ref => (this.scrollViewRef = ref)}
+                                    onScroll={this.handleOnScroll}
                         >
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe600;</Text>
-                                <Text>微信好友</Text>
+                                <TouchableOpacity onPress={this.onSharePress1}>
+                                    <Text style={styles.fontCss}>&#xe600;</Text>
+                                    <Text>微信好友</Text>
+                                </TouchableOpacity>
+
                             </View>
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe601;</Text>
-                                <Text>微信朋友圈</Text>
+                                <TouchableOpacity onPress={this.onSharePress2}>
+                                    <Text style={styles.fontCss}>&#xe601;</Text>
+                                    <Text>微信朋友圈</Text>
+                                </TouchableOpacity>
                             </View>
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe641;</Text>
-                                <Text>QQ好友</Text>
+                                <TouchableOpacity onPress={this.onSharePress3}>
+                                    <Text style={styles.fontCss}>&#xe641;</Text>
+                                    <Text>QQ好友</Text>
+                                </TouchableOpacity>
                             </View>
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe674;</Text>
-                                <Text>QQ空间</Text>
+                                <TouchableOpacity onPress={this.onSharePress4}>
+                                    <Text style={styles.fontCss}>&#xe674;</Text>
+                                    <Text>QQ空间</Text>
+                                </TouchableOpacity>
                             </View>
 
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe61f;</Text>
-                                <Text>系统分享</Text>
+                                <TouchableOpacity onPress={this.onSharePress5}>
+                                    <Text style={styles.fontCss}>&#xe61f;</Text>
+                                    <Text>系统分享</Text>
+                                </TouchableOpacity>
                             </View>
                             <View style={styles.scrollableModalContent1}>
-                                <Text style={styles.fontCss}>&#xe744;</Text>
-                                <Text>复制链接</Text>
+                                <TouchableOpacity onPress={this.onSharePress6.bind(this)}>
+                                    <Text style={styles.fontCss}>&#xe744;</Text>
+                                    <Text>复制链接</Text>
+                                </TouchableOpacity>
                             </View>
                         </ScrollView>
-                        <TouchableOpacity style={styles.cancelToucha} onPress={() => this.setState({ visibleModal: null })}>
+                        <TouchableOpacity style={styles.cancelToucha}
+                                          onPress={() => this.setState({visibleModal: null})}>
                             <Text style={styles.cancelText}>取消</Text>
                         </TouchableOpacity>
                     </View>
@@ -124,22 +238,22 @@ const styles = StyleSheet.create({
     },
     scrollableModalContent1: {
         height: 150,
-        width:Dimensions.get('window').width/4,
+        width: Dimensions.get('window').width / 4,
         backgroundColor: "#f2f2f2",
         alignItems: "center",
         justifyContent: "center"
     },
-    fontCss:{
-        fontFamily:'iconfont',
-        fontSize:50
+    fontCss: {
+        fontFamily: 'iconfont',
+        fontSize: 50
     },
-    cancelToucha:{
-        backgroundColor:'#f8f8f8',
-        height:50,
+    cancelToucha: {
+        backgroundColor: '#f8f8f8',
+        height: 50,
         alignItems: "center",
         justifyContent: "center"
     },
-    cancelText:{
+    cancelText: {
         fontSize: 18,
         textAlign: 'center',
         fontWeight: 'bold'
